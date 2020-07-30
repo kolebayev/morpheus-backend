f56f436fa78a9a3f00f2919ca9fb5e4174798d5b
Ilya Kolebayev <kolebayev@gmail.com>
2020-07-30 12:39:05 +0300

    Init commit + add express server

--- src/index.js
+++ src/index.js
@@ -1,8 +1,11 @@
 
 const express = require('express')
 const app = express()
+const cors = require('cors')
 const port = 8000
 
-app.get('/', (req, res) => res.send('Hello World!'))
+app.use(cors())
+
+app.post('/', (req, res) => res.send({message: 'Hello from express app!'}))
 
 app.listen(port, () => console.log(`Example app listening at http://localhost:${port}!`))
