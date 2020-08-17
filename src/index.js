a5c15f3b443e9c92d4a412437a7e3d0b8dedc457
Ilya Kolebayev <kolebayev@gmail.com>
2020-08-17 21:07:38 +0300

    Fix server response

--- src/index.js
+++ src/index.js
@@ -1,61 +1,60 @@
-console.clear();
+console.clear()
 
-const express = require("express");
-const app = express();
-const cors = require("cors");
-const bodyParser = require("body-parser");
-const Az = require("az");
-const date = require("date-fns");
+const express = require('express')
+const app = express()
+const cors = require('cors')
+const bodyParser = require('body-parser')
+const Az = require('az')
+const date = require('date-fns')
+const locale = require('date-fns/locale')
 
-const port = 8000;
+const port = 8000
 
-app.use(cors());
-app.use(bodyParser.json({ limit: "100mb" }));
-app.use(bodyParser.urlencoded({ extended: true, limit: "100mb" }));
+app.use(cors())
+app.use(bodyParser.json({ limit: '100mb' }))
+app.use(bodyParser.urlencoded({ extended: true, limit: '100mb' }))
 
-app.post("/analyze", express.json(), (req, res) => {
-  const data = req.body;
-  const { post, NMbr, GNdr, messages } = data;
-  const startDate = date.parseISO(data.startDate);
-  const endDate = date.parseISO(data.endDate);
-  let azData = [];
+app.post('/analyze', express.json(), (req, res) => {
+  const data = req.body
+  const { post, NMbr, GNdr, messages } = data
+  console.log(post, NMbr, GNdr)
+  const startDate = date.parseISO(data.startDate)
+  const endDate = date.parseISO(data.endDate)
+  let azData = []
   for (let i = 0; i < messages.length; i++) {
     if (
       (date.parseISO(messages[i].date) > startDate) &
       (date.parseISO(messages[i].date) < endDate)
     ) {
-      azData.push(messages[i].text);
+      azData.push(messages[i])
     }
   }
 
-  console.log(azData);
+  console.log(azData.length)
 
   Az.Morph.init(() => {
-    let words = [];
-    azData.forEach(async (word) => {
-      let result = Az.Morph(word);
-
-      let stat = [];
-      if ((result[0] !== undefined) & (result.length !== 0)) {
-        if (("tag" in result[0]) & (result[0].tag !== undefined)) {
-          stat = result[0].tag.stat;
-          console.log(result[0]);
-          (stat.indexOf(post) > -1) &
-            (stat.indexOf(NMbr) > -1) &
-            (stat.indexOf(GNdr) > -1) && words.push(word);
+    let words = []
+    azData.forEach((word, index) => {
+      let result = Az.Morph(word.text)
+      if (result.length !== 0) {
+        let arr = result[0].tag.stat
+        let arr2 = result[0].tag.flex
+        if (arr.includes(post) & arr.includes(GNdr) & arr2.includes(NMbr)) {
+          words.push({
+            text: word.text,
+            date: date.format(new Date(word.date), 'dd MMMM yyyy,  HH:mm', {
+              locale: locale.ru,
+            }),
+          })
         }
       }
+    })
 
-      // console.log(word, typeof stat, stat);
-    });
-
-    // console.log(x[0].tag.stat);
-    res.send({ lol: words });
-    console.log("————————————————");
-  });
-  // console.log(messages.length);
-});
+    res.send({ words })
+    console.log('————————————————')
+  })
+})
 
 app.listen(port, () =>
   console.log(`Example app listening at http://localhost:${port}!`)
-);
+)
