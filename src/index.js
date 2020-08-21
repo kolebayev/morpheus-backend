1b45329225c89609287809c0c9ebfa5e03f01c7f
Ilya Kolebayev <kolebayev@gmail.com>
2020-08-21 20:16:01 +0300

    Divide getUserMessages between client and server

--- src/index.js
+++ src/index.js
@@ -20,43 +20,65 @@ app.post('/analyze', express.json(), (req, res) => {
 
   const startDate = date.parse(data.rangeStart, 'dd/MM/yyyy', new Date())
   const endDate = date.parse(data.rangeEnd, 'dd/MM/yyyy', new Date())
-  // console.log(typeof startDate, startDate)
 
-  // console.log(
-  //   typeof date.parseISO(filteredMessages[0].date),
-  //   date.parseISO(filteredMessages[0].date)
-  // )
-  console.log('filteredMessages', filteredMessages)
-  // let azData = []
-  // for (let i = 0; i < filteredMessages.length; i++) {
-  //   if (
-  //     (date.parseISO(filteredMessages[i].date) > startDate) &
-  //     (date.parseISO(filteredMessages[i].date) < endDate)
-  //   ) {
-  //     azData.push(filteredMessages[i])
-  //   }
-  // }
+  // удаляет ненужные ключи из каждого объекта сообщений
+  const validKeys = ['date', 'text']
+  filteredMessages.forEach((el) => {
+    Object.keys(el).forEach((key) => validKeys.includes(key) || delete el[key])
+  })
 
-  // console.log(azData.length)
+  // разбивает сообщения на слова и создает новый пословный массив объектов
+  // очищает сообщения от знаков препинания и цифр, оставляет только русские буквы
+  let messagesOneByOne = []
+  filteredMessages.forEach((message) => {
+    let words = message.text.split(' ')
+    const leaveOnlyLetters = new RegExp(/[^а-яА-Я-]+/g)
+    messagesOneByOne.push(
+      ...words.map((word) => {
+        return {
+          date: message.date,
+          text: word.replace(leaveOnlyLetters, ''),
+        }
+      })
+    )
+  })
+
+  let clearedMessages = messagesOneByOne.filter((item) => {
+    return item.text !== ''
+  })
+
+  // console.log(clearedMessages)
+
+  let azData = []
+  for (let i = 0; i < clearedMessages.length; i++) {
+    if (
+      (date.parseISO(clearedMessages[i].date) > startDate) &
+      (date.parseISO(clearedMessages[i].date) < endDate)
+    ) {
+      azData.push(clearedMessages[i])
+    }
+  }
+
+  console.log(azData.length)
 
   Az.Morph.init(() => {
-    // let words = []
-    // azData.forEach((word, index) => {
-    //   let result = Az.Morph(word.text)
-    //   if (result.length !== 0) {
-    //     let arr = result[0].tag.stat
-    //     let arr2 = result[0].tag.flex
-    //     if (arr.includes(post) & arr.includes(GNdr) & arr2.includes(NMbr)) {
-    //       words.push({
-    //         text: word.text,
-    //         date: date.format(new Date(word.date), 'dd MMMM yyyy,  HH:mm', {
-    //           locale: locale.ru,
-    //         }),
-    //       })
-    //     }
-    //   }
-    // })
-    const words = ['response']
+    let words = []
+    azData.forEach((word, index) => {
+      let result = Az.Morph(word.text)
+      if (result.length !== 0) {
+        let arr = result[0].tag.stat
+        let arr2 = result[0].tag.flex
+        if (arr.includes(post) & arr.includes(GNdr) & arr2.includes(NMbr)) {
+          words.push({
+            text: word.text,
+            date: date.format(new Date(word.date), 'dd MMMM yyyy,  HH:mm', {
+              locale: locale.ru,
+            }),
+          })
+        }
+      }
+    })
+
     res.send({ words })
     console.log('————————————————')
   })
